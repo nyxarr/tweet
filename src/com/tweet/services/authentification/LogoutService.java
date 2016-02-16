4cd27179372f133282879256d2cb87b82b945425
nyxar <nyxar75@gmail.com>
2016-02-16 23:58:42 +0100

    Added Logout service + test

---
+++ src/com/tweet/services/authentification/LogoutService.java
@@ -0,0 +1,35 @@
+package com.tweet.services.authentification;
+
+import java.sql.SQLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.tweet.services.ServicesTools;
+import com.tweet.services.tools.AuthentificationTools;
+
+public class LogoutService {
+	public static JSONObject logoutUser(String key) {
+		if (key == null) {
+			return ServicesTools.error("Wrong arguments.", 0);
+		}
+
+		try {
+			if (AuthentificationTools.checkSession(key)) {
+				AuthentificationTools.deleteSession(key);
+			} else {
+                return ServicesTools.error("The session does not exist.", 1);
+            }
+
+            JSONObject response = new JSONObject();
+            response.put("service", "logout");
+            return response;
+		} catch (JSONException e) {
+            return(ServicesTools.error("JSON Problem : "+e.getMessage(),100));
+        } catch (SQLException e) {
+            return(ServicesTools.error("SQL Problem : " + e.getMessage(), 101));
+        } catch (ClassNotFoundException e) {
+            return ServicesTools.error(e.getMessage(), 102);
+        }
+	}
+}
