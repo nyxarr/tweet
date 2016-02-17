c4979bbf443e60c4c5ab033ee8aaa080bc27c4c4
nourdine <nyxar90@gmail.com>
2016-02-17 23:25:18 +0100

    merge

--- src/com/tweet/bd/DBStatic.java
+++ src/com/tweet/bd/DBStatic.java
@@ -7,14 +7,15 @@ import java.sql.SQLException;
 import com.tweet.services.tools.Database;
 
 public class DBStatic {
-	public static String mysql_host = "localhost";
-	public static String mysql_db = "test";
-	public static String mysql_username = "root";
-	public static String mysql_password = "berber";
+	private static String mysql_host = "132.227.201.129:33306";
+	public static String mysql_db = "gr3_guenfissi";
+	public static String mysql_username = "gr3_guenfissi";
+	public static String mysql_password = "XUf3KU";
 	public static boolean mysql_pooling = false;
 	
-	public static Connection getConnection(Database database) throws SQLException {
+	public static Connection getConnection(Database database) throws SQLException, ClassNotFoundException {
 		if (DBStatic.mysql_pooling == false) {
+			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(
 					"jdbc:mysql://" + DBStatic.mysql_host + "/" + DBStatic.mysql_db,
 					DBStatic.mysql_username,
