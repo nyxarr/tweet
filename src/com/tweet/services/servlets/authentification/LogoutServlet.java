66469ce2b04c6ed03ec0aee31d7c580ad15e5ed6
nyxar <nyxar75@gmail.com>
2016-02-17 22:00:26 +0100

    Added LogoutServlet + Updated README

---
+++ src/com/tweet/services/servlets/authentification/LogoutServlet.java
@@ -0,0 +1,29 @@
+package com.tweet.services.servlets.authentification;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import com.tweet.services.authentification.LogoutService;
+
+public class LogoutServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		String key = req.getParameter("key");
+		
+		JSONObject json = LogoutService.logoutUser(key);
+		
+		PrintWriter out = resp.getWriter();
+		out.println(json);
+	}
+}
