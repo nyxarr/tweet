3cfadd94b2bf1c0b42843c87801624987a63ce5d
nyxarr <nourdine.guenfissi@gmail.com>
2016-02-25 00:16:36 +0100

    Update AddComment service

--- src/com/tweet/services/comments/AddCommentService.java
+++ src/com/tweet/services/comments/AddCommentService.java
@@ -1,23 +1,48 @@
 package com.tweet.services.comments;
 
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.bson.Document;
 import org.json.JSONObject;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.tweet.services.ServicesTools;
+import com.tweet.services.tools.AuthentificationTools;
 
 public class AddCommentService {
 	public static JSONObject addComment(String key, String comment) {
 		JSONObject json = new JSONObject();
-		MongoClient mongo = new MongoClient("132.227.201.129", 27130);
-		MongoDatabase mongoDatabase = mongo.getDatabase("tweet");
 		
-		Document commentDoc = new Document();
-		commentDoc.append("user", comment);
-		commentDoc.append("data", comment);
-		
-		mongoDatabase.getCollection("comments").insertOne(commentDoc);
+		try {
+			boolean sessionExists = AuthentificationTools.checkSession(key);
+			
+			if (!sessionExists) {
+				return ServicesTools.error("You are not logged in!", 0);
+			}
+			
+			MongoClient mongo = new MongoClient("localhost", 27017); //132.227.201.129:27130
+			MongoDatabase mongoDatabase = mongo.getDatabase("tweet");
+			
+			Document commentDoc = new Document();
+			int userId = AuthentificationTools.getIdUserBySession(key);
+			Calendar cal = Calendar.getInstance();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            String postDate = dateFormat.format(cal.getTime());
+			
+			commentDoc.append("user", userId);
+			commentDoc.append("data", comment);
+			commentDoc.append("post_date", postDate);
+			
+			mongoDatabase.getCollection("comments").insertOne(commentDoc);
+			mongo.close();
+		} catch (ClassNotFoundException e) {
+			return ServicesTools.error(e.getMessage(), 1);
+		} catch (SQLException e) {
+			return ServicesTools.error(e.getMessage(), 2);
+		}
 		
 		return json;
 	}
--- src/com/tweet/services/tests/CommentsTest.java
+++ src/com/tweet/services/tests/CommentsTest.java
@@ -3,17 +3,18 @@ package com.tweet.services.tests;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.tweet.services.ServicesTools;
 import com.tweet.services.authentification.LoginService;
 import com.tweet.services.comments.AddCommentService;
 
 public class CommentsTest {
 	public static void main(String [] args) {
 		try {
-		JSONObject json = LoginService.loginUser("test", "azerty");
+		JSONObject json = LoginService.loginUser("test1", "azerty");
 		
-		AddCommentService.addComment(json.getString("key"), "test comment");
+		AddCommentService.addComment(json.getString("key"), "Test ajout commentaire");
 		} catch (JSONException e) {
-			e.printStackTrace();
+			ServicesTools.error(e.getMessage(), 0);
 		}
 	}
 }
