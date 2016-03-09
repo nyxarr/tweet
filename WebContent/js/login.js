59c43c6bc4139cd6289938aa9ae83e7cf921f169
nyxar <nyxar75@gmail.com>
2016-03-10 00:01:37 +0100

    Update front

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	$('.show-login').click(function() {
-		$('.login-box').show();
 		$('#shadow-popup').show();
+		$('.login-box').show();
+	});
+	
+	$('#shadow-popup').click(function() {
+		$(this).hide();
+        $('.login-box').css("height", "250px");
+		$('.login-box').hide();
+		$("#login-form").show();
+		$("#register-form").hide();
 	});
 
 	$('#login-submit').click(function() {
@@ -9,7 +17,43 @@ $(document).ready(function() {
 			"/tweet/login",
 			{ username: $('#username').val(), password: $('#password').val() }
 		).done(function (data) {
-			alert(data.key);
+			var key = data.key;
+			$.get("/tweet/comment/get",
+					{ key: key }
+			).done(function (data) {
+				if (data.error) {
+					alert(data.error.message);
+					return;
+				}
+				$('.login-box').hide();
+				$('#shadow-popup').hide();
+			});
 		});
+	});
+	
+	$( "#register" ).on( "click", function() {
+        $('.login-box').animate({
+            height: "350px"},
+            500);
+	    $( "#login-form" ).fadeOut( "slow", function(){
+	       $( "#register-form" ).fadeIn( "slow" );
+	      });
+	});
+	
+	$("#register-submit").click(function() {
+		$.get(
+            "/tweet/register",
+            {
+                firstname: $('#firstname-register').val(),
+                lastname: $('#lastname-register').val(),
+                username: $('#username-register').val(),
+                password: $('#password-register').val(),
+                email: $('#email-register').val()
+            }
+        ).done(function (data) {
+            if (data.error) {
+                alert(data.error.message);
+            }
+        });
 	});
 });
