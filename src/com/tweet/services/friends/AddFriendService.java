84595e041207874d130db8e67cc88e791742b8b7
nyxarr <nourdine.guenfissi@gmail.com>
2016-03-13 22:44:21 +0100

    Finished AddFriendService

--- src/com/tweet/services/friends/AddFriendService.java
+++ src/com/tweet/services/friends/AddFriendService.java
@@ -1,11 +1,56 @@
 package com.tweet.services.friends;
 
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+
 import org.json.JSONObject;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import com.tweet.services.ServicesTools;
+import com.tweet.services.tools.AuthentificationTools;
 
 public class AddFriendService {
 	public static JSONObject addFriend(String key, int friendId) {
-		JSONObject json = new JSONObject();
-		
-		return json;
+		try {
+			if (key == null || new Integer(friendId) == null) {
+				return ServicesTools.error("Wrong arguments", 0);
+			}
+			
+			boolean sessionExists = AuthentificationTools.checkSession(key);
+			
+			if (!sessionExists) {
+				return ServicesTools.error("You are not logged in!", 1);
+			}
+			
+			JSONObject json = new JSONObject();
+			
+			int userId = AuthentificationTools.getIdUserBySession(key);
+			String friendName = AuthentificationTools.getUsernameById(friendId);
+			
+			Mongo mongo = new Mongo("localhost", 27017);
+			DB mongoDatabase = mongo.getDB("test");
+			DBCollection friends = mongoDatabase.getCollection("friends");
+			
+			BasicDBObject user = new BasicDBObject("user_id", userId);
+			BasicDBObject friendDoc = new BasicDBObject();
+			
+			friendDoc.put("user_id", friendId);
+			friendDoc.put("username", friendName);
+			
+			BasicDBObject updateFriends = new BasicDBObject("$push", new BasicDBObject("friends", friendDoc));
+			
+			friends.update(user, updateFriends, true, false);
+			
+			return json;
+		} catch (SQLException e) {
+			return ServicesTools.error(e.getMessage(), 100);
+		} catch (ClassNotFoundException e) {
+			return ServicesTools.error(e.getMessage(), 101);
+		} catch (UnknownHostException e) {
+			return ServicesTools.error(e.getMessage(), 102);
+		}
 	}
 }
