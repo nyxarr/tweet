b3d6f5bf0796f961fb3a9dd893f87998a190adfe
nyxarr <nourdine.guenfissi@gmail.com>
2016-03-16 22:19:51 +0100

    Update exception error codes

--- src/com/tweet/services/ServicesTools.java
+++ src/com/tweet/services/ServicesTools.java
@@ -7,6 +7,11 @@ public class ServicesTools {
 	public static int WRONG_ARG_ERROR = 0;
 	public static int LOGIN_ERROR = 1;
 	public static int JSON_ERROR = 2;
+	
+	public static int SQL_EXCEPTION = 100;
+	public static int CLASS_NOT_FOUND_EXCEPTION = 101;
+	public static int JSON_EXCEPTION = 102;
+	public static int UNKNOWN_HOST_EXCEPTION = 103;
 
 	public static JSONObject error(String message, int errorCode) {
 		JSONObject errResult = new JSONObject();
