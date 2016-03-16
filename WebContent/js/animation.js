e2e67daecdb55411b8e6d908aec9491d7f1c0176
nyxarr <nourdine.guenfissi@gmail.com>
2016-03-16 23:39:55 +0100

    delete animation.js

--- WebContent/js/animation.js
+++
@@ -1,7 +0,0 @@
-$('#leftside-menu').mouseenter(function(event) {
-	$('#leftside-menu').animate({left: "+=260px"}, 350)
-});
-
-$('#leftside-menu').mouseleave(function(event) {
-	$('#leftside-menu').animate({left: "-=260px"}, 500)
-});
